d06ac795b1aa09acb7e14e1d2089faa8ec5aee8d
samueljcatania <samueljcatania@gmail.com>
2024-01-11 19:17:36 -0500

    Updated resume and about section.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function About() {
                 </h3>
                 <div className='border-2 w-40 md:w-52 mb-10 border-black dark:border-dark-theme-text'></div>
                 <p className='font-nunito text-justify dark:text-dark-theme-text'>
-                    I’m a third-year bachelor's student pursuing an Honours Specialization in Computer Science at
+                    I’m a fourth-year bachelor's student pursuing an Honours Specialization in Computer Science at
                     Western University, with a passion for exploring the latest trends and topics in all things machine
                     learning, full-stack web development, and software engineering. I love the creative freedom and
                     flexibility that comes with building websites and web applications, and I'm constantly exploring new
@@ -19,7 +19,7 @@ function About() {
                 </p>
                 <a className='bg-black hover:bg-opacity-75 text-white font-bold py-2 px-4 rounded dark:bg-white
                              dark:text-black dark:hover:bg-opacity-75 duration-200 dark:duration-200 mt-10'
-                   href={require('../assets/resume/Samuel-Catania_Resume.pdf')}
+                   href={require('../assets/resume/Samuel-Catania.pdf')}
                    target='_blank'
                    rel='noopener noreferrer'>
                     Resume
